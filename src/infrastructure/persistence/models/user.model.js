be72235085f9e273555ee30b5db2d45cd1c9deed
OwenMalo <owen.sanchez@jala.university>
2025-10-26 15:18:17 -0600

    feat: base registry

---
+++ src/infrastructure/persistence/models/user.model.js
@@ -0,0 +1,38 @@
+import { DataTypes, Model } from 'sequelize';
+import sequelize from '../../config/database.js';
+
+export class UserModel extends Model {}
+
+UserModel.init(
+  {
+    uid: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true,
+    },
+    firebase_uid: {
+      type: DataTypes.STRING(128),
+      allowNull: false,
+      unique: true,
+    },
+    email: {
+      type: DataTypes.STRING(255),
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true,
+      },
+    },
+    full_name: {
+      type: DataTypes.STRING(255),
+      allowNull: true,
+    },
+  },
+  {
+    sequelize,
+    modelName: 'User',
+    tableName: 'users',
+    timestamps: true, // Enable timestamps for created_at and updated_at
+    freezeTableName: true,
+  }
+);
